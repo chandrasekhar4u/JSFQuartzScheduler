fb28edfbd3c6e6d4cf327f1cf08a33a9118ef0a1
chandrasekhar4u-admin <chandu624@gmail.com>
2015-09-26 01:25:09 +0530

    formatted

--- JSFQuartzScheduler/src/main/java/com/quartz/local/jobs/JobB.java
+++ JSFQuartzScheduler/src/main/java/com/quartz/local/jobs/JobB.java
@@ -1,13 +1,23 @@
+/*
+ * 
+ */
 package com.quartz.local.jobs;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+/**
+ * The Class JobB.
+ */
 public class JobB implements Job {
 
+	/*
+	 * (non-Javadoc)
+	 * @see org.quartz.Job#execute(org.quartz.JobExecutionContext)
+	 */
 	@Override
-	public void execute(JobExecutionContext context)
+	public void execute(final JobExecutionContext context)
 			throws JobExecutionException {
 		System.out.println("Job B is runing");
 
